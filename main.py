7771ee8f3402bf84db280344d1a815587c40e757
Hourgund <nikitacepikov7@gmail.com>
2023-05-23 20:51:53 +0300

    Did basket and cheeked it

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from Entity.milk import Milk
 from Entity.orange import Orange
 from Entity.bread import Bread
+from Container.basket import Basket
 
 
 def main():
+    basket = Basket()
+
     br = Bread()
     o = Orange()
     m = Milk(1, 4.2, 5.5)
 
-    print(br)
-    print(o)
-    print(m)
+    basket.add(br)
+    basket.add(m)
+    basket.add(o)
+
+    print(f"size = {basket.size}")
+
+    print(basket)
 
 
 if __name__ == "__main__":
