65494de824eaf3e4b8f70e457111659d938e4599
Hourgund <nikitacepikov7@gmail.com>
2023-05-23 20:03:26 +0300

    Initial commit

---
+++ Entity/milk.py
@@ -0,0 +1,34 @@
+class Milk:
+    def __init__(self, volume=100, fat=1000, money=0):
+        self.__volume = volume
+        self.__fat = fat
+        self.__money = money
+
+    @property
+    def volume(self):
+        return self.__volume
+
+    @volume.setter
+    def volume(self, volume):
+        self.__volume = volume
+
+    @property
+    def fat(self):
+        return self.__fat
+
+    @fat.setter
+    def fat(self, fat):
+        self.__fat = fat
+
+    @property
+    def money(self):
+        return self.__money
+
+    @money.setter
+    def money(self, money):
+        self.__money = money
+
+    def __str__(self):
+        return (f"Milk: Volume = {self.__volume},"
+                f"Fat = {self.__fat},"
+                f"Money = {self.__money}")
---
+++ Entity/orange.py
@@ -0,0 +1,34 @@
+class Orange:
+    def __init__(self, diameter=100, vitamin=1000, cost=0):
+        self.__diameter = diameter
+        self.__vitamin = vitamin
+        self.__cost = cost
+
+    @property
+    def diameter(self):
+        return self.__diameter
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self.__diameter = diameter
+
+    @property
+    def vitamin(self):
+        return self.__vitamin
+
+    @vitamin.setter
+    def vitamin(self, vitamin):
+        self.__vitamin = vitamin
+
+    @property
+    def cost(self):
+        return self.__cost
+
+    @cost.setter
+    def cost(self, cost):
+        self.__cost = cost
+
+    def __str__(self):
+        return (f"Orange: Diameter = {self.__diameter},"
+                f"Vitamin C = {self.__vitamin},"
+                f"Cost = {self.__cost}")
